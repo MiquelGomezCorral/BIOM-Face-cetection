a5c71590cb68dae6593c3444660324f0d653a6f7
Miquel Gómez <miquelgc2003@gmail.com>
2026-03-09 20:41:12 +0100

    Image anotator

--- src/data/crops.py
+++ src/data/crops.py
@@ -2,33 +2,42 @@ import cv2
 import numpy as np
 from src.config import Configuration
 
-def get_all_image_crops(CONFIG: Configuration, image_path: str):
+def get_all_image_crops(CONFIG: Configuration, image_path: str = None, img: np.ndarray = None):
     crops = []
-    if CONFIG.gray_scale:
-        img = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
-    else:
-        img = cv2.imread(image_path)
+    if img is None:
+        if CONFIG.gray_scale:
+            img = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+        else:
+            img = cv2.imread(image_path)
 
-
+    current_scale = 1.0
+    iteration = 0
+    
     while img.shape[0] > CONFIG.crop_size and img.shape[1] > CONFIG.crop_size:
-        crops.extend(get_image_crops(img, CONFIG.stride, CONFIG.crop_size))
-        img = cv2.resize(
-            img, 
-            dsize=(
-                int(img.shape[0] * CONFIG.subsample_factor), 
-                int(img.shape[1] * CONFIG.subsample_factor)
-            ), 
-            interpolation=cv2.INTER_AREA
-        )
+        crops.extend(get_image_crops(img, CONFIG.stride, CONFIG.crop_size, current_scale))
+        
+        new_w = int(img.shape[1] * CONFIG.subsample_factor)
+        new_h = int(img.shape[0] * CONFIG.subsample_factor)
+        img = cv2.resize(img, (new_w, new_h), interpolation=cv2.INTER_AREA)
+        
+        current_scale /= CONFIG.subsample_factor 
+        iteration += 1
     return crops
 
-def get_image_crops(img: np.ndarray, stride: int, crop_size: int):
+def get_image_crops(img: np.ndarray, stride: int, crop_size: int, scale: float):
     h, w = img.shape[:2]
     crops = []
     for i in range(0, h - crop_size + 1, stride):
         for j in range(0, w - crop_size + 1, stride):
-            crops.append(img[
-                i:i + crop_size, 
-                j:j + crop_size
-            ])
+            crops.append({
+                "x": int(j * scale),
+                "y": int(i * scale),
+                "w": int(crop_size * scale),
+                "h": int(crop_size * scale),
+                "img": img[
+                    i:i + crop_size, 
+                    j:j + crop_size
+                ],
+                
+            })
     return crops
